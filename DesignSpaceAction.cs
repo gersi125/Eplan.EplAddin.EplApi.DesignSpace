dc4e9f6e859e3e9358c0b7ecf63de0f603f45fbf
dev2 <dev2@test.com>
2020-05-22 06:50:06 +0200

    Commented out the copy paragraph of the Eplan Projects.
    Started working on the write into file of the Eplan Project's componenets

--- DesignSpaceAction.cs
+++ DesignSpaceAction.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.DataModel;
-
 
 namespace Eplan.EplAddin.EplApi.DesignSpace
 {
@@ -31,52 +31,91 @@ namespace Eplan.EplAddin.EplApi.DesignSpace
                     throw new Exception($"Project is not exclusive: {project.ProjectLinkFilePath}");
                 }
 
-                //WriteDownPages(project);
-                String strElkPath = project.ProjectLinkFilePath;
-                String prName = project.ProjectName;
-
-                pm.CopyProject(strElkPath, @"C:\TEMP\" + prName + ".elk", ProjectManager.CopyMode.All);
+                WriteDownPages(project);
+                //String strElkPath = project.ProjectLinkFilePath;
+                //String prName = project.ProjectName;
+                //pm.CopyProject(strElkPath, @"C:\TEMP\" + prName + ".elk", ProjectManager.CopyMode.All);
 
             }
             return true;
         }
-    }
-}
 
 
-       /* private void WriteDownPages(Project project)
+
+        private void WriteDownPages(Project project)
         {
-            var pagesGroups = project.Pages.GroupBy(obj => obj.Properties.DESIGNATION_LOCATION);
+            int no_pages     = 0;
+            int no_functions = 0;
+            int no_plcs      = 0;
+
+            var pr_name      = project.ProjectName.ToString();
+            var pr_type      = project.TypeOfProject.ToString();
+            var pagesGroups  = project.Pages.GroupBy(obj => obj.Properties.DESIGNATION_LOCATION);
+
+            StreamWriter ws;
+            FileInfo epinfo = new FileInfo(@"C:\TEMP\" + pr_name + "_Info.txt");
+            ws = epinfo.AppendText();
+          
+            ws.WriteLine("Eplan Project Name: " + pr_name);
+            ws.WriteLine("========================================");
+            ws.WriteLine("Project Type: " + pr_type);
+            ws.WriteLine("========================================");
+            ws.WriteLine("Number of Pages:     " + no_pages);
+            ws.WriteLine("Number of Functions: " + no_functions);
+            ws.WriteLine("Number of PLCs:      " + no_plcs);
+            ws.WriteLine("========================================\n" );
 
             foreach (var pageGroup in pagesGroups)
             {
                 string location = pageGroup.Key;
-                Console.WriteLine("+ " + location);
-                HashSet<string> functions = new HashSet<string>();
-                HashSet<string> func_descs = new HashSet<string>();
-                HashSet<string> plcs = new HashSet<string>();
+               // ws.WriteLine("+ " + location);
+                HashSet<string> p_names     = new HashSet<string>();
+                HashSet<string> p_types     = new HashSet<string>();
+                HashSet<string> plcs        = new HashSet<string>();
+                HashSet<string> plcs_ranks  = new HashSet<string>();
+                HashSet<string> functions   = new HashSet<string>();
+                HashSet<string> func_descs  = new HashSet<string>();
 
                 foreach (Page page in pageGroup)
                 {
-                    var function = page.Properties.DESIGNATION_PLANT;
-                    var func_desc = page.ToStringIdentifier();
-                    var plc = page.PLCs.ToString();
-
+                    var function   = page.Properties.DESIGNATION_PLANT;
+                    var func_desc  = page.ToStringIdentifier();
+                    var plc        = page.PLCs.ToString();
+                    var p_name     = page.Properties.PAGE_NAME.ToString();
+                    var p_type     = page.Properties.PAGE_TYPE.ToString();
+ 
+                    p_names.Add(p_name);
+                    p_types.Add(p_type);
                     functions.Add(function);
                     func_descs.Add(func_desc);
                     plcs.Add(plc);
-                }
 
-                foreach (var function in functions)
+                }
+                foreach (var p_name in p_names)
                 {
-                    foreach (var func_desc in func_descs)
-                    {   
-                        Console.WriteLine("   =" + function + "(" + func_desc + ")");
-                        foreach (var plc in plcs)
+                    foreach( var p_type in p_types){
+
+                        ws.WriteLine("Page: " + p_name + " || Page Type: " + p_type + "\n");
+                       
+                        foreach (var function in functions)
                         {
-                            Console.WriteLine("PLC: " + plc);
+                            foreach (var func_desc in func_descs)
+                            {   
+                                ws.WriteLine(" - Function: " + function + " (" + func_desc + ")");
+                                foreach (var plc in plcs)
+                                { 
+                                        ws.WriteLine("  - PLC: " + plc + "\n");
+                                        no_plcs += 1;
+                                }
+                            }
+                            
+
                         }
                     }
+    
                 }
+            }
 
-            }*/
+        }
+    }
+}
